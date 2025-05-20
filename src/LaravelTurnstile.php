2464d72ea04c2ed1aa79d3d17ea37b210e0a4563
Manikantasripadi <59179052+Manikantasripadi@users.noreply.github.com>
2025-05-20 01:27:59 +0000

    Fix styling

--- src/LaravelTurnstile.php
+++ src/LaravelTurnstile.php
@@ -2,8 +2,8 @@
 
 namespace Neoistone\LaravelTurnstile;
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
 use Neoistone\LaravelTurnstile\Exceptions\SecretKeyNotFoundException;
 use Neoistone\LaravelTurnstile\Exceptions\UnkownErrorOccuredException;
 
