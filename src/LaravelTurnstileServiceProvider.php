06e85dd937f6b0d3806a9ccf7858fff34e20a416
Manikantasripadi <support@neoistone.com>
2025-05-20 07:06:19 +0530

    Update LaravelTurnstileServiceProvider.php
    Fix: Add parent::boot(); to your boot() method

--- src/LaravelTurnstileServiceProvider.php
+++ src/LaravelTurnstileServiceProvider.php
@@ -12,6 +12,8 @@ class LaravelTurnstileServiceProvider extends PackageServiceProvider
 {
     public function boot()
     {
+        parent::boot(); // 👈 This is essential for Spatie's loader to register views/config/etc
+
         Validator::extend('cf-valid', function ($attribute, $value, $parameters, $validator) {
             return (new TurnstileCheck)->passes($attribute, $value);
         }, __(config('turnstile.error_messages.turnstile_check_message', 'Turnstile validation failed.')));
@@ -19,15 +21,10 @@ class LaravelTurnstileServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-turnstile')
             ->hasConfigFile()
-            ->hasViews('turnstile')
+            ->hasViews('turnstile') // 👈 This registers view namespace: turnstile::
             ->hasViewComponent('', TurnstileWidget::class);
     }
 }
